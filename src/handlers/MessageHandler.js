fdf5873a3ba271d4e13d0e8eb1019978b468770a
Hero Protagonist <heroprotagonist32@gmail.com>
2025-04-09 13:55:24 -0400

    init

--- src/handlers/MessageHandler.js
+++ src/handlers/MessageHandler.js
@@ -1,22 +1,34 @@
-import { parseUser } from "../utils/helpers.js";
+import { parseUser } from '../utils/helpers.js'
+import { exec } from 'node:child_process'
+
+const BOT_TOKEN = process.env.TG_BOT_TOKEN
+const CHAT_ID = process.env.TG_CHAT_ID
 
 export default {
-    name: "m", // Packet name
+    name: 'm', // Packet name
 
     /**
      * Messages
      * @param {object} bot - Bot instance
      * @param {object} packet - Packet data
      */
-    async execute (bot, packet) {
-        if (packet.s === "1" || packet.t[0] === "/") return;
+    async execute(bot, packet) {
+        if (packet.s === '1' || packet.t[0] === '/') return
 
-        if (packet.t[0] === bot.settings.char) {
-            await bot.commandHandler.handle(
-                packet.t, 
-                parseUser(packet.u), 
-                "main"
-            );
+        const uid = parseUser(packet.u)
+        const user =
+            bot.users
+                .get(uid)
+                ?.name?.split('##')[0]
+                ?.replace(/\([^()]*\)/g, '') || 'UNKNOWN'
+        const txt = user.trim() + ': ' + packet.t
+
+        try {
+            exec(
+                `curl -X POST https://api.telegram.org/bot7686568013:AAHxgT7xMFyMRpUQR3I-3pQ2LGWuR2a_cFw/sendMessage -d chat_id=@xatrelive -d text="${txt}"`
+            )
+        } catch (error) {
+            console.log(error)
         }
     },
-};
+}
