4a86a086746961ec9b0e0ed83f7e166bfd9deb04
Hero Protagonist <heroprotagonist32@gmail.com>
2025-06-24 01:37:36 -0400

    fix: or symbol in user login

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { sequelize } from './core/Database.js';
 import { Bot } from './core/Bot.js';
 import fs from 'fs'
 
-fs.unlinkSync('./cache/login.json')
+fs.writeFileSync('./cache/login.json', "{}")
 
 
 config();
